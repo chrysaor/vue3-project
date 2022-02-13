d857c863ac9f7080a68daf41232f373cdbea697b
jwkim <chrysaor@naver.com>
2022-02-14 01:38:00 +0900

    initial commit

---
+++ src/pages/todos/_id.vue
@@ -0,0 +1,141 @@
+<template>
+    <h1>To-Do Page</h1>
+    <div v-if="loading">
+        Loading ...
+    </div>
+    <form
+        @submit.prevent="onSave"
+        v-else>
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group">
+                    <label>Subject</label>
+                    <input
+                        v-model="todo.subject"
+                        type="text"
+                        class="form-control">
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="form-group">
+                    <label>Status</label>
+                    <div>
+                        <button 
+                            class="btn"
+                            type="button"
+                            :class="todo.completed ? 'btn-success' : 'btn-danger'"
+                            @click="toggleTodoStatus">
+                            {{ todo.completed ? 'Completed' : 'Incompleted '}}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button
+            type="submit"
+            class="btn btn-primary"
+            :disabled="!todoUpdated">
+            Save
+        </button>
+        <button
+            @click="moveToTodoListPage"
+            class="btn btn-outline-dark ml-2">
+            Cancel
+        </button>
+    </form>
+    <Toast
+        v-if="showToast"
+        :message="toastMessage"
+        :type="toastAlertType"/>
+</template>
+
+<script>
+import { useRoute, useRouter } from 'vue-router';
+import { computed, ref, } from 'vue';
+import axios from 'axios';
+import _ from 'lodash';
+import Toast from '@/components/Toast.vue';
+import { useToast } from '@/hooks/toast';
+
+export default {
+    components: {
+        Toast
+    },
+    setup() {
+        const route = useRoute();
+        const router = useRouter();
+        const todo = ref(null);
+        const originalTodo = ref(null);
+        const loading = ref(true);
+        const todoId = route.params.id;
+
+        // Toast
+        const {
+            toastMessage,
+            toastAlertType,
+            showToast,
+            triggerToast,
+        } = useToast();
+
+        const getTodo = async () => {
+            try {
+                const res = await axios.get(`http://localhost:3000/todos/${todoId}`)
+                todo.value = { ...res.data };
+                originalTodo.value = { ...res.data };
+
+                loading.value = false;
+            } catch (err) {
+                console.log(err);
+                triggerToast('Something went wrong!', 'danger');                                
+            }
+
+        };
+
+        const todoUpdated = computed(() => {
+            return !_.isEqual(todo.value, originalTodo.value);
+        });
+
+        const toggleTodoStatus = () => {
+            todo.value.completed = !todo.value.completed;
+        };
+
+        const moveToTodoListPage = () => {
+            router.push({
+                name: 'Todos'
+            });
+        }
+
+        const onSave = async () => {
+            try {
+                const res = await axios.put(`http://localhost:3000/todos/${todoId}`, {
+                    subject: todo.value.subject,
+                    completed: todo.value.completed
+                });
+
+                originalTodo.value = { ...res.data };
+                triggerToast('To-do object is updated!!');
+            } catch (err) {
+                triggerToast('Something went wrong!', 'danger');                
+            }
+        };
+
+        getTodo();
+
+        return {
+            todo,
+            loading,
+            toggleTodoStatus,
+            moveToTodoListPage,
+            onSave,
+            todoUpdated,
+            showToast,
+            toastMessage,
+            toastAlertType,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/todos/index.vue
@@ -0,0 +1,198 @@
+<template>
+  <div>
+    <h2>To-Do List</h2>
+    <input
+        class="form-control"
+        type="text"
+        v-model="searchText"
+        placeholder="Search"
+        @keyup.enter="searchTodo"
+    >
+    <hr />
+
+    <TodoSimpleForm @add-todo="addTodo" />
+    <div style="color: red">{{ error }}</div>
+
+    <div v-if="!todos.length">
+      There is nothing to display.
+    </div>
+
+    <TodoList 
+      :todos="todos"
+      @toggle-todo="toggleTodo"
+      @delete-todo="deleteTodo"
+    />
+
+    <hr />
+
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li v-if="currentPage === 1" class="page-item disabled">
+          <a style="cursor: pointer" class="page-link" @click="getTodos(currentPage - 1)">
+            Previous
+          </a>
+        </li>
+        <li v-else-if="currentPage !== 1" class="page-item">
+          <a style="cursor: pointer" class="page-link" @click="getTodos(currentPage - 1)">
+            Previous
+          </a>
+        </li>
+        <li
+          v-for="page in numberOfPages"
+          :key="page"
+          class="page-item"
+          :class="currentPage === page ? 'active' : ''"
+        >
+          <a style="cursor: pointer" class="page-link" @click="getTodos(page)">{{page}}</a>
+        </li>
+        <li v-if="currentPage !== numberOfPages" class="page-item">
+          <a style="cursor: pointer" class="page-link" @click="getTodos(currentPage + 1)">Next</a>
+        </li>
+        <li v-else-if="currentPage === numberOfPages" class="page-item disabled">
+          <a style="cursor: pointer" class="page-link" @click="getTodos(currentPage + 1)">Next</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+    <Toast
+        v-if="showToast"
+        :message="toastMessage"
+        :type="toastAlertType"/>
+</template>
+
+<script>
+import { ref, computed, watch } from 'vue';
+import TodoSimpleForm from '@/components/TodoSimpleForm.vue';
+import TodoList from '@/components/TodoList.vue';
+import Toast from '@/components/Toast.vue';
+import axios from 'axios';
+import { useToast } from '@/hooks/toast';
+
+export default {
+  components: {
+    TodoSimpleForm,
+    TodoList,
+    Toast,
+  },
+  setup() {
+    const todos = ref([]);
+    const error = ref('');
+    const numberOfTodos = ref(0);
+    const limit = 5;
+    const currentPage = ref(1);
+    const searchText = ref('');
+
+    const {
+      toastMessage,
+      toastAlertType,
+      showToast,
+      triggerToast,
+    } = useToast();
+
+    const numberOfPages = computed(() => {
+      return Math.ceil(numberOfTodos.value/limit);
+    });
+
+    const getTodos = async (page = currentPage.value) => {
+      currentPage.value = page;
+      try {
+        const res = await axios.get(
+          `http://localhost:3000/todos?subject_like=${searchText.value}&_page=${page}&_limit=${limit}&_sort=id&_order=desc`
+        );
+        numberOfTodos.value = res.headers['x-total-count'];
+        todos.value = res.data;
+      } catch (err) {
+        console.log(err);
+        error.value = 'Something went wrong.';
+        triggerToast('Something went wrong', 'danger');
+      }
+    };
+
+    getTodos();
+
+    const addTodo = async (todo) => {
+      error.value = '';
+      try {
+        await axios.post(`http://localhost:3000/todos`, {
+          subject: todo.subject,
+          completed: todo.completed,
+        });
+
+        getTodos(1);
+      } catch (err) {
+        err.value = 'Something went wrong.';
+        triggerToast('Something went wrong', 'danger');
+      }
+    };
+
+    const deleteTodo = async (index) => {
+      const id = todos.value[index].id;
+      error.value = '';
+
+      try {
+        await axios.delete(
+          `http://localhost:3000/todos/${id}`
+        );
+
+        getTodos(1);
+      } catch (err) {
+        error.value = 'Something went wrong.';
+        triggerToast('Something went wrong', 'danger');
+      }
+    };
+
+    const toggleTodo = async (index, checked) => {
+        console.log(checked);
+        error.value = '';
+        const id = todos.value[index].id;
+      try {
+        await axios.patch(`http://localhost:3000/todos/${id}`, {
+          completed: checked
+        })
+        todos.value[index].completed = checked
+      } catch (err) {
+        error.value = 'Something went wrong.';
+        triggerToast('Something went wrong', 'danger');
+      }
+    };
+
+    let timeout = null;
+
+    const searchTodo = () => {
+      clearTimeout(timeout);
+      getTodos(1);
+    };
+
+    watch(searchText, () => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        getTodos(1);
+      }, 2000);
+    });
+
+    return {
+      todos,
+      addTodo,
+      getTodos,
+      deleteTodo,
+      toggleTodo,
+      searchText,
+      searchTodo,
+      error,
+      numberOfPages,
+      currentPage,
+      showToast,
+      toastMessage,
+      toastAlertType,
+      triggerToast,
+    };
+  }
+}
+</script>
+
+<style>
+  .todo {
+    color: gray;
+    text-decoration: line-through;
+  }
+</style>
